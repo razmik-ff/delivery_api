9e49782afdaa0e0af8b24b644277834a38486c52
Razmik Poghosyan <rpoghosyan@fireflies.am>
2023-06-26 22:31:48 +0400

    Integrated alembic

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-engine = create_engine(
-    "postgresql+psycopg2://user_delivery:pwd_delivery@localhost/delivery"
-)
+POSTGRES_DB_URL = "postgresql+psycopg2://user_delivery:pwd_delivery@localhost/delivery"
+
+engine = create_engine(POSTGRES_DB_URL)
 Base = declarative_base()
 SessionLocal = sessionmaker(bind=engine)
